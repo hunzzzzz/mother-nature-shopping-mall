0ad352eb12556c132d74cee911c003bc992c5c97
hunzz <hunzz.study@gmail.com>
2024-12-23 13:19:11 +0900

    api 서비스들을 위한 기본 프로젝트 생성

---
+++ services/api/build.gradle.kts
@@ -0,0 +1,66 @@
+val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
+bootJar.enabled = false
+
+extra["springCloudVersion"] = "2024.0.0"
+
+plugins {
+	kotlin("jvm") version "2.1.0"
+	kotlin("plugin.spring") version "2.1.0"
+	kotlin("plugin.jpa") version "2.1.0"
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+allprojects{
+	group = "com.nature.mother"
+	version = "0.0.1-SNAPSHOT"
+
+	repositories {
+		mavenCentral()
+	}
+}
+
+subprojects {
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+	apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+
+	dependencies {
+		// kotlin
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		// spring cloud
+		implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+		implementation("org.springframework.cloud:spring-cloud-starter-bus-kafka")
+		implementation("org.springframework.cloud:spring-cloud-starter-config")
+		implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+		// test
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	}
+
+	dependencyManagement {
+		imports {
+			mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+		}
+	}
+
+	kotlin {
+		compilerOptions {
+			freeCompilerArgs.addAll("-Xjsr305=strict")
+		}
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+}
